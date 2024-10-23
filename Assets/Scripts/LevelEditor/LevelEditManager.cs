a4ecb8aafa63734209d9df91a3ce7635efa57b00
Ronja <ronja_mattsson@outlook.com>
2024-10-23 16:15:04 +0200

    Added basic structure for for editor

--- Assets/Scripts/LevelEditor/LevelEditManager.cs
+++ Assets/Scripts/LevelEditor/LevelEditManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class LevelEditManager : MonoBehaviour
+public enum ClickMode { TOGGLE, ADD, REMOVE };
+public enum GridLayers { TERRAIN, UNIT};
+
+public delegate void EditMode();
+
+public class LevelEditManager : Singleton<LevelEditManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public ClickMode editMethod = ClickMode.TOGGLE;
+    public GridLayers editLayer = GridLayers.TERRAIN;
+    Dictionary<(ClickMode, GridLayers), EditMode> editModes = new();
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        editModes.Clear();
+        editModes.Add((ClickMode.TOGGLE, GridLayers.TERRAIN), () => ToggleGround());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetMouseButtonDown((int)MouseButton.Left))
+        {
+            if (editModes.TryGetValue((editMethod, editLayer), out EditMode currentMode))
+            {
+                currentMode();
+            }
+        }
+    }
+
+    public void ToggleGround()
+    {
+        Debug.Log("We toggling the ground!");
     }
 }
